ab55996fbae4f89e4fc62aa918b90666e749f273
webcoderspeed <webcoderspeed@gmail.com>
2022-09-06 21:47:51 +0530

    fetching data from json server

--- src/components/BlogList/BlogList.jsx
+++ src/components/BlogList/BlogList.jsx
@@ -1,64 +1,54 @@
 import React from 'react';
-import './BlogList.css'
+import './BlogList.css';
 
 const BlogList = ({ blogs, title, setBlogs }) => {
-
   const handleDelete = (id) => {
     console.log(`Blog Id - ${id}`);
-    const newBlogs = blogs.filter(blog => blog.id !== id)
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
-  }
+  };
 
   return (
     <>
-    <h1>{title}</h1>
-      {
-        blogs && blogs.length ? (
-          <>
-            {
-              blogs.map(blog => {
-                return (
-                  <article className='blog-card' key={blog.id}>
-                    <img
-                      src={blog.image}
-                      alt={blog.title}
-                      className='blog-img'
-                    />
+      <h1>{title}</h1>
+      {blogs && blogs.length ? (
+        <>
+          {blogs.map((blog) => {
+            return (
+              <article className='blog-card' key={blog.id}>
+                <img src={blog.image} alt={blog.title} className='blog-img' />
 
-                    <div className='blog-card-body'>
-                      <h3>{blog.category}</h3>
-                      <h1>{blog.title}</h1>
-                      <p>
-                        {blog.body.substring(0, 200) + '...'}
-                      </p>
-                      <div className='blog-author-section'>
-                        <div className='blog-author-info'>
-                          <img
-                            src={blog.author.avatar}
-                            alt=''
-                          />
-                          <div>
-                            <h5>{blog.author.name}</h5>
-                            <p>{blog.createdAt}</p>
-                          </div>
-                        </div>
-
-                        <button className='blog-cta'>Read more</button>
-                        <button className='blog-cta'
-                          onClick={() => handleDelete(blog.id)}
-                        >Delete</button>
+                <div className='blog-card-body'>
+                  <h3>{blog.category}</h3>
+                  <h1>{blog.title}</h1>
+                  <p>{blog.body.substring(0, 200) + '...'}</p>
+                  <div className='blog-author-section'>
+                    <div className='blog-author-info'>
+                      <img src={blog.author.avatar} alt='' />
+                      <div>
+                        <h5>{blog.author.name}</h5>
+                        <p>{blog.createdAt}</p>
                       </div>
                     </div>
-                  </article>
-                )
-              })
-            }
-          </>
-        ) : <h1>No blogs exists</h1>
-      }
+
+                    <button className='blog-cta'>Read more</button>
+                    <button
+                      className='blog-cta'
+                      onClick={() => handleDelete(blog.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </article>
+            );
+          })}
+        </>
+      ) : (
+        <h1>No blogs exists</h1>
+      )}
     </>
   );
 };
 
 export default BlogList;
-
