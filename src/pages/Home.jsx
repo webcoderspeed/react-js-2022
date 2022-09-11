b0e0bde9b0dc53de1942eb436a96952220cb5962
webcoderspeed <webcoderspeed@gmail.com>
2022-09-11 21:27:34 +0530

    conditional rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,22 @@ import BlogList from '../components/BlogList/BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => res.json())
-      .then((data) => setBlogs(data));
+      .then((data) => {
+        setBlogs(data);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <section className='home-container'>
-      <BlogList blogs={blogs} title='All Blogs' setBlogs={setBlogs} />
+      {
+        loading ? <h1>Loading...</h1> : <BlogList blogs={blogs} title='All Blogs' setBlogs={setBlogs} />
+      }
     </section>
   );
 };
